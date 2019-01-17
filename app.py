15fa75bcef94ca7473ff5b448f2b7d53eb2896f9
Simon Deeb <simon19-meet@meet.mit.edu>
2019-01-17 09:57:15 -0800

    finished signup and login

--- app.py
+++ app.py
@@ -1,21 +1,55 @@
 from flask import *
-from flask import render_template
+from flask import session as login_session
 from model import *
-from databases import *
+from database import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("home.html")
+    a=login_session['FirstName']
+    return render_template("home.html",a=a)
 
-@app.route('/signup')
+@app.route('/signup',methods=['GET','POST'])
 def signUp():
-    return render_template("signup.html")
+    if request.method == 'POST':
+        firstName=request.form['fname']
+        lastName=request.form['lname']
+        age=request.form['age']
+        gender=request.form['gender']
+        email=request.form['email']
+        password=request.form['password']
+        AddStudent(firstName,lastName,age,gender,email,password)
+        return redirect(url_for('homepage'))
+    else:
+        return render_template("signup.html")
 
-@app.route('/teachersignup')
+@app.route('/teachersignup',methods=['GET','POST'])
 def teacherSignUp():
+    if request.method == 'POST':
+        firstName=request.form['fname']
+        lastName=request.form['lname']
+        age=request.form['age']
+        gender=request.form['gender']
+        email=request.form['email']
+        password=request.form['password']
+        subject=request.form['subject']
+        yearsOfExp=request.form['expyears']
+        available=True
+        AddTeacher(firstName,lastName,age,gender,email,password,subject,yearsOfExp,available)
+        return redirect(url_for('homepage'))
     return render_template("teachersignup.html")
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    txt=""
+    if request.method == 'POST':
+        email=request.form['email']
+        password=request.form['password']
+        student = GetStudentByEmail(email)
+        return redirect(url_for('homepage'))
+    else:
+        return render_template("login.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
--- database.py
+++ database.py
@@ -1,12 +1,23 @@
-from models import *
+from model import *
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///cats.db')
+engine = create_engine('sqlite:///accounts.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def function(parameter):
-    pass
+def AddStudent(firstName,lastName,age,gender,email,password):
+    st=Student(FirstName=firstName,LastName=lastName,Age=age,Email=email,Password=password)
+    session.add(st)
+    session.commit()
+
+def AddTeacher(firstName,lastName,age,gender,email,password,subject,yearsOfExperience,available):
+    t=Teacher(FirstName=firstName,LastName=lastName,Age=age,Gender=gender,Email=email,Password=password,YearsOfExperience=yearsOfExperience,Available=available)
+    session.add(t)
+    session.commit()
+
+def GetStudentByEmail(email):
+    stud=session.query(Student).filter_by(Email=email).first()
+    return stud
